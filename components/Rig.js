e144dbe2f271d2b8af258f39e2e199379bd38422
triinjuss <triinjuss@icloud.com>
2020-01-30 11:43:22 +0200

    kurseri asukoht

--- components/Rig.js
+++ components/Rig.js
@@ -1,23 +1,19 @@
+
 export default {
-  mounted() {
-    AFRAME.registerComponent('listener', {
-      tick: function () {}
-    });
-  },
   template: `
-        <a-entity id="rig" movement-controls="constrainToNavMesh: true;" position="0 0 0">
-          <a-camera position="0 1.6 0" listener fov="80" look-controls="pointerLockEnabled: true;" wasd-controls="false">
+        <a-entity id="rig" position="0 0 0" rotation="0 90 0">
+          <a-camera>
               <a-entity 
                 cursor="fuse:true, fuseTimeout: 50;" raycaster="far: 10; objects: .hover"
                 position="0 0 -1"
                 geometry="primitive: circle; radius: 0.005;"
-                material="color: white; shader: flat; opacity: 0.5"
+                material="color: white; shader: flat; opacity: 0.7"
                 >
               </a-entity>
               <slot />
           </a-camera>
-          <a-entity position="0 0 4" oculus-go-controls="model: false"></a-entity>
-          <a-entity laser-controls="hand: right"></a-entity>
+      <a-entity position="0 0 5" oculus-go-controls="model: false"></a-entity>
+      <a-entity laser-controls="hand: right"></a-entity>
         </a-entity>
     `
 };
